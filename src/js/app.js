2384c9bdb69cbf736f34a8ba94ae337d5c95ca69
Oleg Khvostenko <xboctehko@yandex.ru>
2023-08-23 22:01:35 +0200

    Components

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,4 @@
-//import togglePopover from "./popover";
+import togglePopover from "./popover";
 
 const btn = document.querySelector('.btn');
-
-function togglePopover(elem, title = '', text = '') {
-  const popover = document.querySelector('.popover');
-  if (!popover) { return ; }
-
-  popover.querySelector('.popover_title').textContent = title || popover.querySelector('.popover_title').textContent;
-  popover.querySelector('.popover_text').textContent = text || popover.querySelector('.popover_text').textContent;
-  if (popover.style.display === 'inline-block') {
-    popover.style.display = 'none';
-  } else {
-    const { left, top } = elem.getBoundingClientRect();
-    popover.style.display = 'inline-block';
-    popover.style.left = left + (elem.offsetWidth - popover.offsetWidth) / 2 + 'px';
-    popover.style.top = top - popover.offsetHeight - 4 + 'px';
-  }
-}
-
 btn.addEventListener('click', () => togglePopover(btn));
