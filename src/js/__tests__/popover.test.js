0b6ea7b9fd03ad12caf4854dd5ff38ae7b2a3f05
Oleg Khvostenko <xboctehko@yandex.ru>
2023-08-23 22:50:22 +0200

    Test

--- src/js/__tests__/popover.test.js
+++ src/js/__tests__/popover.test.js
@@ -2,7 +2,11 @@ import togglePopover from "../popover";
 
 test('some test', () => {
   const div = document.createElement('div');
+  div.textContent = 'Test';
+  div.style.display = 'inline-block';
+  div.style.margin = '100px 60px';
   document.body.appendChild(div);
+  div.bindToDOM();
   togglePopover(div);
   const popover = document.querySelector('.popover');
   popover.bindToDOM();
